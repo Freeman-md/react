4bf34dd91803f92484a39101943f9ac02d5f8284
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-05-15 12:51:11 +0100

    feat: integrate Tailwind CSS and refactor counter app

--- beginner/counter-app/src/App.tsx
+++ beginner/counter-app/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState<number>(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const increment = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event)
+
+    setCounter((prev: number) => prev += 1)
+  }
+
+  const decrement = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event)
+
+    setCounter((prev: number) => {
+      if (prev > 0) {
+        return prev -= 1
+      }
+
+      return prev
+    })
+  }
+
+  return <main className="w-dvw h-dvh grid align-middle place-content-center">
+
+    <section className="flex items-center space-x-6">
+    <button className="btn" onClick={decrement}>-</button>
+    <span className="text-4xl font-semibold">{counter}</span>
+    <button className="btn" onClick={increment}>+</button>
+    </section>
+
+  </main>;
 }
 
-export default App
+export default App;
